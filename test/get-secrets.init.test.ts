3c8578b582c0268194a9f8cc1cd48fb1ea9076f7
shimopino <shimoke4869@gmail.com>
2022-05-10 06:19:27 +0900

    red: UrlIDが無効な場合のテストケースを追加

--- test/get-secrets.init.test.ts
+++ test/get-secrets.init.test.ts
@@ -1,9 +1,17 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 describe('シークレット値を取得するための結合テスト', () => {
   it.todo('シークレット値を取得できる');
 
   it.todo('シークレット値がDBに登録されていない場合、エラーが返される');
 
-  it.todo('指定したUrlIDが無効な場合、エラーが返される');
+  it('指定したUrlIDが無効な場合、エラーが返される', async () => {
+    const response = await request.get('/api/v1/secrets/123_short');
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      name: 'UrlIdValidationError',
+      message: 'UrlIDが短すぎます',
+    });
+  });
 });
