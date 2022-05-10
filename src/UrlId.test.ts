686473cdf288652cdb95cae62055ec21916e4dbc
shimopino <shimoke4869@gmail.com>
2022-05-11 03:30:43 +0900

    UrlIdの作成に関する期待値を追加

--- src/UrlId.test.ts
+++ src/UrlId.test.ts
@@ -1,5 +1,9 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { UrlId } from './UrlId';
 
 describe('UrlId Tests', () => {
-  it('should create a instance of UrlId', () => {});
+  it('should create a instance of UrlId', () => {
+    expect(new UrlId('123jjshdfajslhfd')).toBeInstanceOf(UrlId);
+  });
+  });
 });
