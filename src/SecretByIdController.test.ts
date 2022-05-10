969180c5ebccb8baad31bc6fb5c570bb0c966a98
shimopino <shimoke4869@gmail.com>
2022-05-11 03:22:02 +0900

    コントローラーから返されるエラーオブジェクトに関する期待値を追加

--- src/SecretByIdController.test.ts
+++ src/SecretByIdController.test.ts
@@ -1,5 +1,13 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import { SecretByIdController } from './SecretByIdController';
+import { UrlIdValidationError } from './UrlIdValidationError';
 
 describe('SecretByIdController Tests', () => {
-  it('should throw an error if the urlId is too short', () => {});
+  it('should throw an error if the urlId is too short', () => {
+    const secretByIdController = new SecretByIdController();
+    secretByIdController.retrieveSecretById(req, res, next);
+
+    expect(next).toBeCalledTimes(1);
+    expect(next).toBeCalledWith(new UrlIdValidationError('UrlIDが短すぎます'));
+  });
 });
