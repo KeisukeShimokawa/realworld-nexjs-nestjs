97fbbb4ad66be0f67b1a1f8992a177267b694c9d
shimopino <shimoke4869@gmail.com>
2022-05-11 03:34:24 +0900

    エラーレスポンスを作成するための実装を追加

--- src/ErrorHandler.ts
+++ src/ErrorHandler.ts
@@ -5,4 +5,9 @@ export function errorHandler(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  res.status(400).json({
+    name: error.name,
+    message: error.message,
+  });
+}
