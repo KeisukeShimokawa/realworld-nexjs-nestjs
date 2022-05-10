008106beca8fac782166e12964907a49b352fe27
shimopino <shimoke4869@gmail.com>
2022-05-11 03:27:15 +0900

    エラーの読み出しを追加

--- src/SecretByIdController.ts
+++ src/SecretByIdController.ts
@@ -1,5 +1,11 @@
+import { NextFunction } from 'connect';
+import { Request, Response } from 'express';
+import { UrlIdValidationError } from './UrlIdValidationError';
+
 export class SecretByIdController {
-  retrieveSecretById(retrieveSecretById: any) {
-    throw new Error('Method not implemented.');
+  retrieveSecretById(req: Request, res: Response, next: NextFunction) {
+    if (req.params.urlId.length < 10) {
+      next(new UrlIdValidationError('UrlIDが短すぎます'));
+    }
   }
 }
