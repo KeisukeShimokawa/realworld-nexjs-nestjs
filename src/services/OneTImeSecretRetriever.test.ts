49fd7715638277f1fcd5277a85d279015b7ad16c
shimopino <shimoke4869@gmail.com>
2022-05-13 19:57:31 +0900

    GREEN: 依存関係をテストを実行する際に追加

--- src/services/OneTImeSecretRetriever.test.ts
+++ src/services/OneTImeSecretRetriever.test.ts
@@ -1,15 +1,23 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { SecretNotFoundError } from '../domain/errors/SecretNotFoundError';
 import { UrlId } from '../domain/models/UrlId';
 import { OneTimeSecretRetriever } from './OneTImeSecretRetriever';
+import { SecretRepository } from './SecretRepository';
 
 describe('OneTimeSecretRetriver Tests', () => {
   it('should throw an error if the Secret was not found', () => {
-    const oneTimeSecretRetriever = new OneTimeSecretRetriever();
+    const secretRepository: SecretRepository = {
+      getSecretByUrlId: vi.fn().mockResolvedValueOnce(null),
+    };
+    const oneTimeSecretRetriever = new OneTimeSecretRetriever(secretRepository);
 
     const urlId = new UrlId('ajhfdjkfhasdf');
     expect(oneTimeSecretRetriever.retrieveSecretById(urlId)).rejects.toThrow(
       SecretNotFoundError
+    );
+    expect(secretRepository.getSecretByUrlId).toBeCalledTimes(1);
+    expect(secretRepository.getSecretByUrlId).toBeCalledWith(
+      new UrlId('ajhfdjkfhasdf')
     );
   });
 });
