bb7d44aaefb8af565ae8bb26c6731fed9eca6776
shimopino <shimoke4869@gmail.com>
2022-05-13 18:15:07 +0900

    RED: シークレット値が存在しない場合のエラーオブジェクトのテストケースを追加

---
+++ src/SecretNotFoundError.test.ts
@@ -0,0 +1,10 @@
+import { expect, describe, it } from 'vitest';
+
+describe('UrlIdValidationError Tests', () => {
+  it('should create a UrlIdValidationError error', () => {
+    const error = new SecretNotFoundError();
+    expect(error).toBeInstanceOf(SecretNotFoundError);
+    expect(error.name).toBe('SecretNotFoundError');
+    expect(error.message).toBe('Secret was not found in the system');
+  });
+});
