d45fe8222f03985bbabe7071f847af56691bf900
shimopino <shimoke4869@gmail.com>
2022-05-13 18:19:23 +0900

    RED: urlIdからシークレット値を取得するためのメソッドへの呼び出し想定を追加

--- src/rest/SecretByIdController.ts
+++ src/rest/SecretByIdController.ts
@@ -1,12 +1,15 @@
 import { NextFunction } from 'connect';
 import { Request, Response } from 'express';
+import { SecretRetriver } from '../SecretRetriver';
 import { UrlId } from '../UrlId';
 
 export class SecretByIdController {
+  constructor(private secretRetriever: SecretRetriver) {}
 
   async retrieveSecretById(req: Request, res: Response, next: NextFunction) {
     try {
       const urlId = new UrlId(req.params.urlId);
+      const secret = await this.secretRetriever.retrieveSecretById(urlId);
     } catch (error) {
       next(error);
     }
