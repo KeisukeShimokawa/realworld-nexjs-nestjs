042392da391176e7411518fedfd30d08d003dedd
shimopino <shimoke4869@gmail.com>
2022-05-13 18:27:42 +0900

    GREEN: シークレット値が短い場合の検証処理の実装を追加

--- src/Secret.ts
+++ src/Secret.ts
@@ -1,3 +1,8 @@
+import { SecretValidationError } from './SecretValidationError';
+
 export class Secret {
-  constructor(private secret: string) {}
+  constructor(private secret: string) {
+    if (secret.length <= 3)
+      throw new SecretValidationError('Secretが短すぎます');
+  }
 }
