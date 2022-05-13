85ebff552dfde75e8f0723fa52cef4f891f00f42
shimopino <shimoke4869@gmail.com>
2022-05-13 18:30:40 +0900

    RED: SecretNotFoundErrorに対する検証処理を追加

--- src/rest/ErrorHandler.test.ts
+++ src/rest/ErrorHandler.test.ts
@@ -2,6 +2,7 @@ import { Request, request, Response, response } from 'express';
 import { describe, expect, it, vi } from 'vitest';
 import { errorHandler } from './ErrorHandler';
 import { UrlIdValidationError } from '../UrlIdValidationError';
+import { SecretNotFoundError } from '../SecretNotFoundError';
 
 describe('ErrorHandler Tests', () => {
   it('should generate an Error response for a UrlIdValidationError', () => {
@@ -22,6 +23,28 @@ describe('ErrorHandler Tests', () => {
     expect(res.json).toBeCalledWith({
       name: 'UrlIdValidationError',
       message: 'UrlIDが短すぎます',
+    });
+  });
+
+  it('should generate an Error response for a SecretNotFoundError', () => {
+    const error = new SecretNotFoundError();
+    const req: Request = request;
+    req.params = { urlId: 'test' };
+    const res: Response = response;
+    res.status = vi.fn().mockReturnThis();
+    res.json = vi.fn().mockReturnThis();
+    const next = vi.fn();
+
+    errorHandler(error, req, res, next);
+
+    console.log(res);
+    expect(next).toBeCalledTimes(0);
+    expect(res.status).toBeCalledTimes(1);
+    expect(res.status).toBeCalledWith(404);
+    expect(res.json).toBeCalledTimes(1);
+    expect(res.json).toBeCalledWith({
+      name: 'SecretNotFoundError',
+      message: 'Secret was not found in the system',
     });
   });
 });
