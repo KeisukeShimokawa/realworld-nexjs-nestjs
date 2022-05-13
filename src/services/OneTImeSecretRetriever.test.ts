f16f6b49bc0728930d606e584721745dc3dd6a4c
shimopino <shimoke4869@gmail.com>
2022-05-13 19:07:40 +0900

    RED: 作成した実装への参照を追加

--- src/services/OneTImeSecretRetriever.test.ts
+++ src/services/OneTImeSecretRetriever.test.ts
@@ -1,13 +1,14 @@
 import { describe, expect, it } from 'vitest';
 import { SecretNotFoundError } from '../domain/errors/SecretNotFoundError';
 import { UrlId } from '../domain/models/UrlId';
+import { OneTimeSecretRetriever } from './OneTImeSecretRetriever';
 
 describe('OneTimeSecretRetriver Tests', () => {
   it('should throw an error if the Secret was not found', () => {
-    const oneTimeSecretRetriver = new OneTimeSecretRetriver();
+    const oneTimeSecretRetriever = new OneTimeSecretRetriever();
 
     const urlId = new UrlId('ajhfdjkfhasdf');
-    expect(oneTimeSecretRetriver.retrieveSecretByUrlId(urlId)).rejects.toThrow(
+    expect(oneTimeSecretRetriever.retrieveSecretById(urlId)).rejects.toThrow(
       SecretNotFoundError
     );
   });
