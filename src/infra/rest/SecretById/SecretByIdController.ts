951b794b17ab8985155bad238b5ac60209022569
shimopino <shimoke4869@gmail.com>
2022-05-17 00:04:33 +0900

    GREEN: thisのコンテキストを固定する実装を追加

--- src/infra/rest/SecretById/SecretByIdController.ts
+++ src/infra/rest/SecretById/SecretByIdController.ts
@@ -4,7 +4,15 @@ import { SecretRetriver } from '../../../services/SecretRetriver';
 import { UrlId } from '../../../domain/models/UrlId';
 
 export class SecretByIdController {
-  constructor(private secretRetriever: SecretRetriver) {}
+  constructor(private secretRetriever: SecretRetriver) {
+    /**
+     * Routeで関数をExpressに設定する時に、thisのコンテキストが想定したものからずれる
+     * そのため、以下のリンクを参考にインスタンス生成時にコンテキストを固定する
+     *
+     * https://stackoverflow.com/questions/4011793/this-is-undefined-in-javascript-class-methods
+     */
+    this.retrieveSecretById = this.retrieveSecretById.bind(this);
+  }
 
   async retrieveSecretById(req: Request, res: Response, next: NextFunction) {
     try {
