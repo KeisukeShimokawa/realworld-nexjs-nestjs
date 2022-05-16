8beebfe2385ffbe36b8ed27c9d6b7a600d537cbc
shimopino <shimoke4869@gmail.com>
2022-05-16 22:48:23 +0900

    GREEN: MongoDBへの接続の実装を追加

--- src/infra/repositories/MongoSecretRepository.ts
+++ src/infra/repositories/MongoSecretRepository.ts
@@ -1,9 +1,12 @@
+import mongoose from 'mongoose';
 import { Secret } from '../../domain/models/Secret';
 import { UrlId } from '../../domain/models/UrlId';
 import { SecretRepository } from '../../services/SecretRepository';
 
 export class MongoSecretRepository implements SecretRepository {
-  constructor() {}
+  constructor() {
+      mongoose.connect('mongodb://localhost:27017/onetimesecretdb');
+  }
 
   async getSecretByUrlId(urlId: UrlId): Promise<Secret> {
     throw new Error('method not implemented.');
