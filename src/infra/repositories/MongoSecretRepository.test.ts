a3e3d93d9757dfb3ed3121d358727ab2c09d7801
shimopino <shimoke4869@gmail.com>
2022-05-16 22:53:52 +0900

    RED: MongoDBのSecret値を保存するためのスキーマを追加

--- src/infra/repositories/MongoSecretRepository.test.ts
+++ src/infra/repositories/MongoSecretRepository.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it, vi } from 'vitest';
 import mongoose from 'mongoose';
 import { MongoSecretRepository } from './MongoSecretRepository';
+import { UrlId } from '../../domain/models/UrlId';
+import { SecretModel } from './SecretModel';
 
 describe('MongoSecretRepository Tests', () => {
   it('should connect to the database', () => {
@@ -24,5 +26,14 @@ describe('MongoSecretRepository Tests', () => {
   });
 
   it('should return a null object when the secret is not found', async () => {
+    SecretModel.findOne = vi.fn().mockResolvedValueOnce(null);
+    mongoose.connect = vi.fn();
+    mongoose.connection.readyState = 1;
+
+    const urlId = new UrlId('sdjhfaasjaf');
+    const mongoSecretRepository = new MongoSecretRepository();
+
+    expect(await mongoSecretRepository.getSecretByUrlId(urlId)).toBeNull();
+    expect(mongoose.connect).toBeCalledTimes(0);
   });
 });
