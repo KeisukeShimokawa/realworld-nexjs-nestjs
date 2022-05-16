0c1f88f0e4804c7a7fa1bb758e08fa83b4961b7e
shimopino <shimoke4869@gmail.com>
2022-05-16 22:44:24 +0900

    RED: MongoDBへのモックに vi.fn() を使用

--- src/infra/repositories/MongoSecretRepository.test.ts
+++ src/infra/repositories/MongoSecretRepository.test.ts
@@ -1,9 +1,11 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import mongoose from 'mongoose';
 import { MongoSecretRepository } from './MongoSecretRepository';
 
 describe('MongoSecretRepository Tests', () => {
   it('should connect to the database', () => {
+    mongoose.connect = vi.fn();
+
     new MongoSecretRepository();
 
     expect(mongoose.connect).toBeCalledTimes(1);
