a6ca8786fb1e214049d9c6134b78089d0837c985
shimopino <shimoke4869@gmail.com>
2022-05-19 05:51:14 +0900

    GREEN: 作成したバリデーションエラーを使用したtry-ctchを追加

--- src/infra/rest/Secret/SecretController.ts
+++ src/infra/rest/Secret/SecretController.ts
@@ -1,11 +1,16 @@
 import { NextFunction } from 'connect';
 import { Request, Response } from 'express';
+import { RequestValidationError } from './RequestValidationError';
 
 export class SecretController {
   constructor() {}
 
   async storeSecret(req: Request, res: Response, next: NextFunction) {
-    console.log('hi');
-    throw new Error('method not implemented yet');
+    try {
+      if (!req.body)
+        throw new RequestValidationError('Request body is not provided');
+    } catch (error) {
+      next(error);
+    }
   }
 }
