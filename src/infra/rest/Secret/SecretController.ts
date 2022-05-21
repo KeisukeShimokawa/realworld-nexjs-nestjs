d1c3e3d74b2ee44276283d6a59fcfe4e88fdca43
shimopino <shimoke4869@gmail.com>
2022-05-21 10:35:22 +0900

    GREEN: シークレット値が文字列ではない場合の検証処理を追加

--- src/infra/rest/Secret/SecretController.ts
+++ src/infra/rest/Secret/SecretController.ts
@@ -11,6 +11,9 @@ export class SecretController {
     try {
       if (!req.body?.secret)
         throw new RequestValidationError('Request body format is not valid');
+
+      if (typeof req.body?.secret !== 'string')
+        throw new RequestValidationError('Secret is not a string');
     } catch (error) {
       next(error);
     }
