3814e63ff6ae909d5226d983abfbfdde5c69629c
shimopino <shimoke4869@gmail.com>
2022-05-21 11:42:42 +0900

    GREEN: シークレット値を取得するサービスへの依存関係を追加

--- src/infra/rest/Secret/SecretController.test.ts
+++ src/infra/rest/Secret/SecretController.test.ts
@@ -1,6 +1,9 @@
 import { request, Request, response, Response } from 'express';
 import { describe, expect, it, vi } from 'vitest';
 import { SecretValidationError } from '../../../domain/errors/SecretValidationError';
+import { Secret } from '../../../domain/models/Secret';
+import { UrlId } from '../../../domain/models/UrlId';
+import { SecretStorer } from '../../../services/SecretStorer';
 import { RequestValidationError } from './RequestValidationError';
 import { SecretController } from './SecretController';
 
@@ -13,7 +16,10 @@ describe('SecretController Tests', () => {
     const res: Response = response;
     const next = vi.fn();
 
-    const secretByIdController = new SecretController();
+    const secretStorer: SecretStorer = {
+      storeSecretAndUrlId: vi.fn(),
+    };
+    const secretByIdController = new SecretController(secretStorer);
     await secretByIdController.storeSecret(req, res, next);
 
     expect(next).toBeCalledTimes(1);
@@ -30,7 +36,10 @@ describe('SecretController Tests', () => {
     const res: Response = response;
     const next = vi.fn();
 
-    const secretByIdController = new SecretController();
+    const secretStorer: SecretStorer = {
+      storeSecretAndUrlId: vi.fn(),
+    };
+    const secretByIdController = new SecretController(secretStorer);
     await secretByIdController.storeSecret(req, res, next);
 
     expect(next).toBeCalledTimes(1);
@@ -47,7 +56,10 @@ describe('SecretController Tests', () => {
     const res: Response = response;
     const next = vi.fn();
 
-    const secretByIdController = new SecretController();
+    const secretStorer: SecretStorer = {
+      storeSecretAndUrlId: vi.fn(),
+    };
+    const secretByIdController = new SecretController(secretStorer);
     await secretByIdController.storeSecret(req, res, next);
 
     expect(next).toBeCalledTimes(1);
@@ -66,10 +78,19 @@ describe('SecretController Tests', () => {
     res.json = vi.fn().mockReturnThis();
     const next = vi.fn();
 
-    const secretByIdController = new SecretController();
+    const secretStorer: SecretStorer = {
+      storeSecretAndUrlId: vi
+        .fn()
+        .mockResolvedValueOnce(new UrlId('jhsdgfksdjhfas')),
+    };
+    const secretByIdController = new SecretController(secretStorer);
     await secretByIdController.storeSecret(req, res, next);
 
     expect(next).toBeCalledTimes(0);
+    expect(secretStorer.storeSecretAndUrlId).toBeCalledTimes(1);
+    expect(secretStorer.storeSecretAndUrlId).toBeCalledWith(
+      new Secret('myValidSecret22')
+    );
     expect(res.status).toBeCalledTimes(1);
     expect(res.json).toBeCalledTimes(1);
     expect(res.json).toBeCalledWith(new UrlId('jhsdgfksdjhfas'));
