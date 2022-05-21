04502398996787af1015be81a598aebb4a0e0b65
shimopino <shimoke4869@gmail.com>
2022-05-21 10:24:15 +0900

    GREEN: リクエストボディにシークレット値が含まれていない場合はエラー

--- src/infra/rest/Secret/SecretController.test.ts
+++ src/infra/rest/Secret/SecretController.test.ts
@@ -17,4 +17,21 @@ describe('SecretController Tests', () => {
       new RequestValidationError('Request body is not provided')
     );
   });
+
+  it('should throw an error if the secret is not presend in the body', async () => {
+    const req: Request = request;
+    req.body = {
+      asb: 'ajsdhfa',
+    };
+    const res: Response = response;
+    const next = vi.fn();
+
+    const secretByIdController = new SecretController();
+    await secretByIdController.storeSecret(req, res, next);
+
+    expect(next).toBeCalledTimes(1);
+    expect(next).toBeCalledWith(
+      new RequestValidationError('Request body format is not valid')
+    );
+  });
 });
--- src/infra/rest/Secret/SecretController.ts
+++ src/infra/rest/Secret/SecretController.ts
@@ -6,9 +6,15 @@ export class SecretController {
   constructor() {}
 
   async storeSecret(req: Request, res: Response, next: NextFunction) {
+    console.log('req', req.body);
+
     try {
+      // if (req.body && Object.keys(req.body).length === 0)
       if (!req.body)
         throw new RequestValidationError('Request body is not provided');
+
+      if (!req.body?.secret)
+        throw new RequestValidationError('Request body format is not valid');
     } catch (error) {
       next(error);
     }
