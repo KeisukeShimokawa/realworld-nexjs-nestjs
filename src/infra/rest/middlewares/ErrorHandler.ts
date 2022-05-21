6a9f3576ddfd8e2229f249f66738f33b5d2b4fb3
shimopino <shimoke4869@gmail.com>
2022-05-21 10:46:13 +0900

    GREEN: 400系のエラーに対してシークレット値が短い場合の実装を追加

--- src/infra/rest/middlewares/ErrorHandler.ts
+++ src/infra/rest/middlewares/ErrorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { SecretNotFoundError } from '../../../domain/errors/SecretNotFoundError';
+import { SecretValidationError } from '../../../domain/errors/SecretValidationError';
 import { UrlIdValidationError } from '../../../domain/errors/UrlIdValidationError';
 import { RequestValidationError } from '../Secret/RequestValidationError';
 
@@ -16,7 +17,8 @@ export function errorHandler(
     });
   } else if (
     error instanceof UrlIdValidationError ||
-    error instanceof RequestValidationError
+    error instanceof RequestValidationError ||
+    error instanceof SecretValidationError
   ) {
     res.status(400).json({
       name: error.name,
