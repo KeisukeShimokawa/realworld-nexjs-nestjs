ea97c56f893ccb36a339ca1c2ffc4ecbb8ecc323
shimopino <shimoke4869@gmail.com>
2022-05-22 13:51:58 +0900

    RED: トークンを生成するためのクラスのインターフェースを追加

--- src/services/OneTimeSecretStorer.ts
+++ src/services/OneTimeSecretStorer.ts
@@ -1,14 +1,21 @@
 import { Secret } from '../domain/models/Secret';
 import { UrlId } from '../domain/models/UrlId';
 import { SecretStorer } from './SecretStorer';
+import { TokenGenerator } from './TokenGenerator';
 
 export class OneTimeSecretStorer implements SecretStorer {
-  constructor() {}
+  constructor(
+    private tokenGenerator: TokenGenerator
+  ) {}
 
   async storeSecretAndUrlId(secret: Secret): Promise<UrlId> {
     // generate a token for urlId
+    const token = this.tokenGenerator.generateToken();
+
     // create a urlId
+
     // store both
+
     // return urlId only
   }
 }
