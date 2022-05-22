66e2fa9f64dd270763bb8f85daf5aa76b06181f8
shimopino <shimoke4869@gmail.com>
2022-05-22 14:00:25 +0900

    RED: 作成したトークン生成器への依存を追加

--- src/services/OneTimeSecretStorer.test.ts
+++ src/services/OneTimeSecretStorer.test.ts
@@ -1,11 +1,18 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { Secret } from '../domain/models/Secret';
 import { UrlId } from '../domain/models/UrlId';
 import { OneTimeSecretStorer } from './OneTimeSecretStorer';
+import { TokenGenerator } from './TokenGenerator';
 
 describe('OneTimeSecretStorer Tests', () => {
   it('should store a secret in the repository and return a UrlId', async () => {
-    const oneTimeSecretStorer = new OneTimeSecretStorer();
+    const tokenGenerator: TokenGenerator = {
+      generateToken: vi.fn().mockReturnValueOnce('fgashfgkafas'),
+    };
+
+    const oneTimeSecretStorer = new OneTimeSecretStorer(
+      tokenGenerator
+    );
     const secret = new Secret('myValidSecret22');
     const result = await oneTimeSecretStorer.storeSecretAndUrlId(secret);
 
